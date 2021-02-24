a668021464e5fcae22ae58fc4fb011d42a9a5adf
Ryan Matthew Gonzalez <70781659+ryan-gon@users.noreply.github.com>
2021-02-23 22:40:56 -0500

    Update server.js

--- server.js
+++ server.js
@@ -3,13 +3,10 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const path = require("path")
-
-
 const PORT = process.env.PORT || 3000;
-
 const app = express();
 
-// Middleware
+
 app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -20,7 +17,7 @@ require("./routes/APIroutes.js")(app)
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutdb", { useNewUrlParser: true });
 
-// Listen 
+
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
 });
